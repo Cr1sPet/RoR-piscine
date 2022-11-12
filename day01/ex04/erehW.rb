e062e742a54eae6144bd848be83490953b0e3910
CrsPet <serezha.petrov.02@gmail.com>
2022-11-13 01:27:29 +0300

    d01 ex04

---
+++ day01/ex04/erehW.rb
@@ -0,0 +1,36 @@
+#!/usr/bin/env ruby
+def get_states
+  {
+   "Oregon" => "OR",
+   "Alabama" => "AL",
+   "New Jersey" => "NJ",
+   "Colorado" => "CO"
+   }
+end
+
+def get_capitals_cities
+ {
+   "OR" => "Salem",
+   "AL" => "Montgomery",
+   "NJ" => "Trenton",
+   "CO" => "Denver"
+ }
+end
+
+
+def get_state_by_capital capital
+
+ state_key = get_capitals_cities.key(capital)
+
+ get_states.key(state_key) || 'Unknown capital city'
+
+end
+
+def parse argv
+ if argv.length != 1
+   exit
+ end
+ argv[0]
+end
+
+puts get_state_by_capital parse ARGV
