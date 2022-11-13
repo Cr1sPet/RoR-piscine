912d34b865e705e21624b8505641ffaf2207abdf
CrsPet <serezha.petrov.02@gmail.com>
2022-11-13 14:06:35 +0300

    d01 ex05

---
+++ day01/ex05/whereto.rb
@@ -0,0 +1,45 @@
+#!/usr/bin/env ruby
+def get_states
+  {
+   "Oregon" => "OR",
+   "Alabama" => "AL",
+   "New Jersey" => "NJ",
+   "Colorado" => "CO"
+   }
+end
+
+def get_capitals_cities
+ {
+   "OR" => "Salem",
+   "AL" => "Montgomery",
+   "NJ" => "Trenton",
+   "CO" => "Denver"
+ }
+end
+
+def get_result_string elem
+  if get_capitals_cities.has_value? elem
+    akr = get_capitals_cities.key(elem)
+    return "#{elem} is the capital of #{get_states.key(akr)} (akr: #{akr})"
+  end
+  if get_states.has_key? elem
+    akr = get_states[elem]
+    return "#{get_capitals_cities[akr]} is the capital of #{elem} (akr: #{akr})"
+  end
+  "#{elem} is neither a capital city nor a state"
+end
+
+def process arr
+  arr.map{|el| get_result_string el}
+end
+
+def parse argv
+  exit if argv.length != 1
+  arr = argv[0].split ','
+  exit if arr.include? ''
+  arr.map{|el| el.gsub(/\s+/, '').capitalize}.select do |el|
+    el != ''
+  end
+end
+
+puts process (parse ARGV)
