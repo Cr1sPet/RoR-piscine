9d767659ad8dad7fa66ed85ac3f5d3f538f08af4
CrsPet <serezha.petrov.02@gmail.com>
2022-11-14 17:50:17 +0300

    day02 ex01

---
+++ day02/ex01/ex01.rb
@@ -0,0 +1,77 @@
+#!/usr/bin/env ruby
+
+
+class Html
+
+  def Html.obj_count
+    count = 0
+    ObjectSpace.each_object(self){|obj| count += 1}
+  end
+
+  def initialize(page_name)
+    @page_name = page_name
+    head
+  end
+
+  attr_reader :page_name
+
+  def head
+    if File.exist? "#{page_name}.html"
+      raise "A file named #{page_name}.html already exist!"
+    end
+    file = File.new("#{page_name}.html", 'w');
+    file.puts(
+      "<!DOCTYPE html>
+      <html lang='en'>
+        <head>
+          <title>#{page_name}</title>
+          <meta charset='UTF-8'>
+        </head>
+        <body>"
+    )
+    file.close
+  end
+
+  def dump info
+
+    input = File.read "#{page_name}.html"
+    if !input.include? '<body>'
+      raise "There is no body tag in #{page_name}.html"
+    end
+    if input.include? '</body>'
+      raise "Body has already been closed in  #{page_name}.html"
+    end
+
+    file = File.open("#{page_name}.html", 'a')
+
+    file.puts(
+      "<p>#{info}</p>"
+    )
+    file.close
+  end
+
+  def finish
+
+    if (File.read "#{page_name}.html").include? '</body>'
+      raise  "#{page_name}.html has already been closed."
+    end
+
+    file = File.open("#{page_name}.html", 'a')
+
+    file.puts(
+      "</body>
+      </html>"
+    )
+    file.close
+  end
+
+end
+
+
+if $PROGRAM_NAME == __FILE__
+  a = Html.new "generated"
+  10.times{|el| a.dump "test_p_#{el}"}
+  a.finish
+end
+
+# require_relative "ex01.rb"
